41377467f942e4a0d29be7454674c0928de3feac
bima242 <arya_tantular_33rpl@student.smktelkom-mlg.sch.id>
2024-08-19 12:43:17 +0700

    Add files via upload

---
+++ Karyawan.java
@@ -0,0 +1,30 @@
+//object class 
+public class Karyawan {
+    int id;
+    String nama, unit, alamat;
+
+    //Constructor Parameter
+    public Karyawan(int id, String nama, String unit, String alamat) {
+        //variabel = parameter
+        this.id = id;
+        this.nama = nama;
+        this.unit = unit;
+        this.alamat = alamat;
+    }
+
+    //method print
+    public void print() {
+        System.out.println("ID : "+ id);
+        System.out.println("Nama : "+ nama);
+        System.out.println("Unit : "+ unit);
+        System.out.println("Alamat : "+ alamat);
+    }
+   
+    
+    }
+
+
+    
+
+    
+
